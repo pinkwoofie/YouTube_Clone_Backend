7b93149fb3de914183a082f06f913516ae060e42
= <=>
2024-07-15 13:16:54 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ dotenv.config({
 })
 
 connectDB()
+.then( () => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch( (err) => {
+    console.log("Mongodb db connection failed  !! ", err)
+})
 
 
 
